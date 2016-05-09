1815fecfd1c54f550b0a2d19eb87a71af468b2f4
ilokhart <ilokhart@gmail.com>
2016-05-09 02:23:23 +0000

    init

---
+++ html/popup.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+	document.getElementById('psbuttonid').addEventListener('click', function(){
+		window.open('https://www.google.com/#q=the+division+pc+gameplay');
+		});
+  });
